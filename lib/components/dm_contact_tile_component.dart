25a5316899c8fa0bef153bb4331c29c188059d5c
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-10-22 12:50:31 -0300

    dotted line and last style change

--- lib/components/dm_contact_tile_component.dart
+++ lib/components/dm_contact_tile_component.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:be_talent_desafio_mobile/constants.dart';
 import 'package:be_talent_desafio_mobile/controllers/home_screen_controller.dart';
+import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/material.dart';
 
 class DmContactTileComponent extends StatefulWidget {
@@ -65,13 +66,11 @@ class _DmContactTileComponentState extends State<DmContactTileComponent> {
       );
     }
 
-    
     final isLast = widget.index == screenController.filteredEmployees.length;
-    print(screenController
-        .filteredEmployees[widget.index - 1].detailsModel.length);
+
     return AnimatedContainer(
       duration: const Duration(milliseconds: 300),
-      height: _expanded ? 200  : 60,
+      height: _expanded ? 200 : 60,
       decoration: BoxDecoration(
         borderRadius: isLast
             ? const BorderRadius.only(
@@ -113,8 +112,10 @@ class _DmContactTileComponentState extends State<DmContactTileComponent> {
               alignment: Alignment.center,
               width: 30, // Largura igual ao ícone no cabeçalho
               child: InkWell(
-                child: const Icon(
-                  Icons.keyboard_arrow_down,
+                child: Icon(
+                  _expanded
+                      ? Icons.keyboard_arrow_up
+                      : Icons.keyboard_arrow_down,
                   color: Pallete.bluePrimary,
                   size: 30,
                 ),
@@ -138,18 +139,28 @@ class _DmContactTileComponentState extends State<DmContactTileComponent> {
                   .filteredEmployees[widget.index - 1].detailsModel
                   .map(
                 (details) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        details.label,
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-                      Text(
-                        details.value.toString(),
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                    ],
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              details.label,
+                              style: Theme.of(context).textTheme.headlineMedium,
+                            ),
+                            Text(
+                              details.value.toString(),
+                              style: Theme.of(context).textTheme.headlineSmall,
+                            ),
+                          ],
+                        ),
+                        const DottedLine(
+                          dashColor: Pallete.gray10,
+                        ),
+                      ],
+                    ),
                   );
                 },
               ).toList(),
